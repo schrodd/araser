4adf1468c752e0036c25a44cf3c2dec7ec2926d2
schrodd <andres_alejandro_rodriguez@hotmail.com>
2023-05-05 15:12:02 -0300

    added metricool tracker

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,23 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
+      <Script strategy='afterInteractive'>
+        {`function loadScript(a){
+            var b=document.getElementsByTagName("head")[0];
+            var c=document.createElement("script");
+            c.type="text/javascript";
+            c.src="https://tracker.metricool.com/resources/be.js";
+            c.onreadystatechange=a;
+            c.onload=a;
+            b.appendChild(c);
+          }
+          loadScript(function(){
+            beTracker.t({hash:"6801246ecc0e92f5618666edd03131e9"})
+          })`}
+      </Script>
       <Head />
       <body>
         <Main />
